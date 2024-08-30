d541c289f086598fc52f2081e4551f3b20c6138d
CodingWithSamuel <samu1800.so@gmail.com>
2024-08-30 15:00:07 -0300

    fix "APROVAR USUARIOS" MENU VAI SER IMPLEMENTADO!!!

--- index.js
+++ index.js
@@ -2,50 +2,50 @@
 /* Menu interativo, If - vereifica qual o tipo de usuario logado, else altera menu conforme o usuario 
 Funciton representa o menu interativo (toggleMenu) */
 
-  const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
-  const adminMenu = document.getElementById("adminMenu");
+const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+const adminMenu = document.getElementById("adminMenu");
 
-  if (loggedInUser) {
+if (loggedInUser) {
 
-      document.getElementById("login-item").style.display = "none";
-      
-      const profilePicContainer = document.getElementById("profile-pic-container");
-      const profilePic = document.getElementById("profile-pic");
-      profilePic.src = loggedInUser.profilePic;
-      profilePicContainer.style.display = "block";
+    document.getElementById("login-item").style.display = "none";
 
-      if (loggedInUser.username === "admin") {
-          adminMenu.innerHTML = `
-              <li><a href="#" onclick="aprovarUsuarios()">Aprovar Usuários</a></li>
+    const profilePicContainer = document.getElementById("profile-pic-container");
+    const profilePic = document.getElementById("profile-pic");
+    profilePic.src = loggedInUser.profilePic;
+    profilePicContainer.style.display = "block";
+
+    if (loggedInUser.username === "admin") {
+        adminMenu.innerHTML = `
+              <li><a href="AprovedUsers/aproveusers.html" onclick="aprovarUsuarios()">Aprovar Usuários</a></li>
               <li><a href="#">Configurações</a></li>
           `;
-      } else {
-          adminMenu.innerHTML = `
+    } else {
+        adminMenu.innerHTML = `
               <li><a href="#">Conta</a></li>
           `;
-      }
-  }
+    }
+}
 
-  function toggleMenu() {
-      const menu = document.getElementById("adminMenu");
-      if (menu.style.display === "none" || menu.style.display === "") {
-          menu.style.display = "block";
-      } else {
-          menu.style.display = "none";
-      }
-  }
+function toggleMenu() {
+    const menu = document.getElementById("adminMenu");
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
 
-      const logoutButton = document.getElementById("logout-btn");
-      logoutButton.addEventListener("click", function() {
-          localStorage.clear();
-          window.location.href = "index.html";
-      });
+const logoutButton = document.getElementById("logout-btn");
+logoutButton.addEventListener("click", function () {
+    localStorage.clear();
+    window.location.href = "index.html";
+});
 
 
 function updateProgressBar(progressBarId, progressPercentId, percent) {
     const progressBar = document.getElementById(progressBarId);
     const progressPercent = document.getElementById(progressPercentId);
-    
+
     progressBar.style.width = percent + '%';
     progressPercent.textContent = percent + '%';
 }
